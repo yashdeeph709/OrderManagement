dd8a2abd135fe53360d9e17a0690c8cb971d0781
yashdeep hinge <yashdeeph709@gmail.com>
2015-02-27 18:54:47 +0530

    angular and node ready

--- js/app.js
+++ js/app.js
@@ -3,9 +3,13 @@ var app=angular.module('OrderManagement',['ngRoute']);
 app.config(function($routeProvider){
     $routeProvider.when('/',{
         templateUrl:'partials/main.html'
-    }).when('/order',{
-        templateUrl:'partials/order.html'
+    }).when('/orders',{
+        templateUrl:'partials/orders.html'
     }).when('/createOrder',{
         templateUrl:'partials/createOrder.html'
+    }).when('/customers',{
+        templateUrl:'partials/customers.html'
+    }).when('/products',{
+        templateUrl:'partials/products.html'
     });
 });
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var http=require('http');
-var express=require('express');
-var app=express();
+var http =  require('http');
+var express = require('express');
+var app = express();
 app.use(express.static('./'));
 app.listen(3000);
