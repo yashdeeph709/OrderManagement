63e5c76703058bb5b57be1396cef645837c1b10c
yashdeep hinge <yashdeeph709@gmail.com>
2015-03-01 04:37:15 +0530

    cards test completed

--- js/app.js
+++ js/app.js
@@ -3,8 +3,12 @@ var app=angular.module('OrderManagement',['ngRoute']);
 app.config(function($routeProvider){
     $routeProvider.when('/',{
         templateUrl:'partials/main.html'
+    }).when('/giftcards',{
+        templateUrl:'partials/giftcards.html'
     }).when('/orders',{
         templateUrl:'partials/orders.html'
+    }).when('/selectProduct',{
+        templateUrl:'partials/selectProduct.html'
     }).when('/createOrder',{
         templateUrl:'partials/createOrder.html'
     }).when('/customers',{
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-var http =  require('http');
 var express = require('express');
 var app = express();
 app.use(express.static('./'));
