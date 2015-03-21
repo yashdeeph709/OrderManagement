c48e448ecd3e72628ffc29457c09b782625e17c3
unknown <yashdeeph709@gmail.com>
2015-03-22 00:08:19 +0530

    server.js comp with heroku

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 var express = require('express');
 var app = express();
-app.use(express.static('./'));
-app.listen(3000);
+
+app.set('port', (process.env.PORT || 5000));
+app.use('/',express.static('./'));
+app.use('/partials',express.static(__dirname+'./partials'));
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
