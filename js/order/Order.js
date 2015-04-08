9dabe273337ebf24a4473fcc460e2b2f562af20b
unknown <yashdeeph709@gmail.com>
2015-04-08 14:37:40 +0530

    Theme change amma!

--- js/order/Order.js
+++ js/order/Order.js
@@ -1,6 +1,8 @@
 var app=angular.module('OrderModule',[]);
 
 app.controller('createOrder', function($scope){
+	$scope.products=[];
+	$scope.itemid=0;
 	$scope.setCustId=function(cid){
 		$scope.cid=cid;
 	}
@@ -9,6 +11,16 @@ app.controller('createOrder', function($scope){
 	}
 	$scope.setAddress=function(){
 		$scope.address=true;
+	}
+	$scope.addItem=function(){
+		$scope.products.push({
+			"itemid":$scope.itemid++,
+			"id":$scope.id,
+			"name":$scope.name,
+			"qty":$scope.qty,
+			"unit":$scope.unit,
+			"total":$scope.total
+		});
 	}
 });
 app.controller('orderlist',function($scope,orders){
